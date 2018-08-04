dfaab7beafa3c4268934343bbc1eea43826f37f9
Pisit Supsri <p.supsri@gmail.com>
2018-08-04 22:53:57 +0700

    build myalbum page

---
+++ src/pages/MyAlbum.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ id }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    id: null
+  }),
+  created () {
+    this.reload()
+  },
+  methods: {
+    reload () {
+      this.id = this.$route.params.id
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/Index'
+import MyAlbum from '@/pages/MyAlbum'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index
-    }
+    { path: '/', component: Index },
+    { path: '/myalbum/:id', component: MyAlbum, meta: { requiresAuth: true } }
   ]
 })
