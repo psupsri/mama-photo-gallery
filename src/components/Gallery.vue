277d6be4ad886a253b8ec8478d6ed0f5f0040e7c
Pisit Supsri <p.supsri@gmail.com>
2018-08-04 21:34:30 +0700

    build gallery option button

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -6,11 +6,35 @@
       :key="index"
       xs3
     >
-      <v-card flat tile>
+      <v-card flat tile
+        class="card"
+      >
         <v-card-media
           :src="photo.url"
           height="190px"
-        ></v-card-media>
+        >
+        </v-card-media>
+        <div class="option fadein">
+          <div class="top-right">
+            <v-btn icon
+              color="white"
+              :ripple="false"
+            >
+              <v-icon color="red accent-2">favorite_border</v-icon>
+            </v-btn>
+            <v-btn icon
+              color="info"
+            >
+              <v-icon>share</v-icon>
+            </v-btn>
+          </div>
+          <div class="bottom-right">
+            <v-btn depressed>
+              <v-icon>input</v-icon>
+              view
+            </v-btn>
+          </div>
+        </div>
       </v-card>
     </v-flex>
   </v-layout>
@@ -38,3 +62,43 @@ export default {
 }
 </script>
 
+<style scoped>
+.option {
+  display: none;
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+.top-right {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.bottom-right {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+.card {
+  cursor: pointer;
+}
+.card:hover .option {
+  display: block;
+}
+@-webkit-keyframes fadeIn {
+  0% {opacity: 0;}
+  100% {opacity: 1;}
+}
+
+@keyframes fadeIn {
+  0% {opacity: 0;}
+  100% {opacity: 1;}
+}
+
+.fadein {
+  -webkit-animation-name: fadeIn;
+  animation-name: fadeIn;
+}
+
+</style>
